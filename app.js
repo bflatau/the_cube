3b67a75b6a415e3296b0d940cc67858d97e32f76
Ben <bflatau@gmail.com>
2024-08-21 00:25:51 -0700

    got timer thingy working...

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ const { Server } = require('socket.io');
 const fetch  = require('node-fetch');
 const { parse, end, toSeconds, pattern } = require('iso8601-duration');
 
-// import fetch from 'node-fetch';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-
-
 
 
 app.use(express.static('./static'));
@@ -25,32 +22,69 @@ app.get('/', (req, res) => {
 ////GET YOUTUBE VIDEO STUFF////
 
 const musicVideos = [
-  'asdfasd', /// VIDEO NAME
-  'asdfasdf', /// VIDEO NAME
+  'eTeg1txDv8w', // Crash Test Dummies (MMMM MMM MMM)
+  'h1Z89zW-8sY', // Our Lady Peace (Innocent)
+  '72JtKmOoiFU', // Eminem (Real Slim Shady)
+  'CduA0TULnow', // Britney Spears (Oops I did it again)
+  'gJLIiF15wjQ', // Spice Grils (Wanabe)
+  'lOfZLb33uCg', // Weird Al (Amish Paradise)
+  'NOG3eus4ZSo', // Smashing Pumpkins (Tonight, Tonight)
+  '3mbBbFH9fAg', // Soundgarden (Blackhole Sun)
+  'dn3j6-yQKWQ', // Nine Inch Nails (Perfect Drug)
+  'ruAi4VBoBSM', // Fatboy Slim (Praise you)
+  'gUhRKVIjJtw', // Puff Daddy (mo money mo problems)
+  '3qVPNONdF58', // Blind Melon (no rain)
+  'jRGrNDV2mKc', // Korn (freak on a leash)
+  'RYnFIRc0k6E', // Limp Bizkit (Rollin)
+  'QtTR-_Klcq8', // Offspring (Pretty Fly for a White Guy)
+
 ]
 
-const randomElement = array[Math.floor(Math.random() * array.length)];
+let timeleft;
+let youtubeVideo;
 
-// const months = ["January", "February", "March", "April", "May", "June", "July"];
+function fakePlayVideo(video){
+  timeleft = video.videoLength;
+  youtubeVideo = video.video;
 
-// const random = Math.floor(Math.random() * months.length);
-// console.log(random, months[random]);
+  io.emit("new youtube video", youtubeVideo)
 
-async function benTest(){
-  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id=Ks-_Mh1QhMc&key=${process.env.YOUTUBE_API_KEY}` );
-  // const body = await response.text();
-  const body = await response.text();
+  const videoTimer = setInterval(function(){
+    timeleft -= 1;
+    console.log('time left', timeleft)
 
-  const bodyObject = JSON.parse(body);
-
-  // console.log(bodyObject.items[0].contentDetails.duration);
-
-  console.log(toSeconds(parse(bodyObject.items[0].contentDetails.duration)));
-
+    if(timeleft <= 1){
+      clearInterval(videoTimer);
+      console.log('song done!');
+      startMusicPlayer();
+    }
+    // timeleft -= 1;
+  }, 1000); 
 }
 
 
-benTest()
+async function getNewSong(){
+  console.log('getting new song');
+  const randomSong = musicVideos[Math.floor(Math.random() * musicVideos.length)];
+  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails&id=${randomSong}&key=${process.env.YOUTUBE_API_KEY}` );
+  const body = await response.text();
+  const bodyObject = JSON.parse(body);
+
+  return {
+    video: randomSong,
+    videoLength: toSeconds(parse(bodyObject.items[0].contentDetails.duration))
+    }
+}
+
+function startMusicPlayer(){
+  getNewSong().then(result => {fakePlayVideo(result)})
+}
+
+startMusicPlayer();
+
+
+
+
 
 
 
@@ -69,13 +103,20 @@ benTest()
 // });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
 
-  socket.on('video length', (msg) => {
-    console.log('this is video length', msg)
-  });
+  socket.emit('youtube video', youtubeVideo);
+  console.log('this is video', youtubeVideo)
+  socket.emit('video time', timeleft)
+  console.log('this is time left', timeleft)
+
+
+  // socket.on('chat message', (msg) => {
+  //   io.emit('chat message', msg);
+  // });
+
+  // socket.on('video length', (msg) => {
+  //   console.log('this is video length', msg)
+  // });
 
 });
 
